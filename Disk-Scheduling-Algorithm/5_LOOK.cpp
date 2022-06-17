e9eb7892744d01598fd3aca1c8a952929fe837a4
Dharaneish V C <82216772+dharaneishvc@users.noreply.github.com>
2022-06-17 19:36:42 +0530

    Create 5_LOOK.cpp

---
+++ Disk-Scheduling-Algorithm/5_LOOK.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void LOOK(int arr[], int head, string direction, int size )
+{
+	int seek_count = 0;
+	int distance, cur_track;
+	vector<int> left, right;
+	vector<int> seek_sequence;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] < head)
+			left.push_back(arr[i]);
+		if (arr[i] > head)
+			right.push_back(arr[i]);
+	}
+
+	sort(left.begin(), left.end());
+	sort(right.begin(), right.end());
+
+	int run = 2;
+	while (run--) {
+		if (direction == "left") {
+			for (int i = left.size() - 1; i >= 0; i--) {
+				cur_track = left[i];
+				seek_sequence.push_back(cur_track);
+				distance = abs(cur_track - head);
+				seek_count += distance;
+				head = cur_track;
+			}
+			direction = "right";
+		}
+		else if (direction == "right") {
+			for (int i = 0; i < right.size(); i++) {
+				cur_track = right[i];
+				seek_sequence.push_back(cur_track);
+				distance = abs(cur_track - head);
+				seek_count += distance;
+				head = cur_track;
+			}
+			direction = "left";
+		}
+	}
+
+	cout << "Total number of seek operations = "<< seek_count << endl;
+
+	cout << "Seek Sequence is" << endl;
+
+	for (int i = 0; i < seek_sequence.size(); i++)
+	{
+		cout << seek_sequence[i] << endl;
+	}
+}
+
+int main()
+{
+    int size, head;
+    cin>>size>>head;
+    int tracks[size];
+    for(int i=0; i<size; i++)
+    {
+        cin>>tracks[i];
+    }
+    string direction;
+    cin>>direction;
+	LOOK(tracks, head, direction, size);
+	return 0;
+}
