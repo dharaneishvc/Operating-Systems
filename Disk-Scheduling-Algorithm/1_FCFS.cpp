40122246185c09d78dd2191715eac71adaf5db6f
Dharaneish V C <82216772+dharaneishvc@users.noreply.github.com>
2022-06-17 15:04:13 +0530

    Create 1_FCFS.cpp

---
+++ Disk-Scheduling-Algorithm/1_FCFS.cpp
@@ -0,0 +1,37 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void FCFS(int arr[], int size, int head)
+{
+	int seek_count = 0;
+	int distance, cur_track;
+
+	for (int i = 0; i < size; i++)
+	{
+		seek_count += abs(arr[i] - head);
+		head = arr[i];
+	}
+
+	cout << "Total number of seek operations = "<< seek_count << endl;
+
+	cout << "Seek Sequence is" << endl;
+
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << endl;
+	}
+}
+
+int main()
+{
+    int size, head;
+    cin>>size>>head;
+    int tracks[size];
+    for(int i=0; i<size; i++)
+    {
+        cin>>tracks[i];
+    }
+
+	FCFS(tracks, size, head);
+	return 0;
+}
