ee8cbbae10c5b691f1e040d4d82965569bf1ac4f
Dharaneish V C <82216772+dharaneishvc@users.noreply.github.com>
2022-06-30 23:25:43 +0530

    Update pthread3.c

--- code2/pthread3.c
+++ code2/pthread3.c
@@ -1,43 +1,45 @@
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
-pthread_t tid[2];
-int counter=0;
-int x=0;
-void* trythis(void* arg)
+#include<pthread.h>
+#include<stdio.h>
+#include<semaphore.h>
+#include<unistd.h> 
+void *fun1();
+ void *fun2();
+ int shared=1; //shared variable
+ sem_t s; //semaphore variable
+ int main()
+ {
+ sem_init(&s,0,1); //initialize semaphore variable - 1st argument is address of variable, 2nd is number of processes sharing semaphore, 3rd argument is the initial value of semaphore variable
+ pthread_t thread1, thread2;
+ pthread_create(&thread1, NULL, fun1, NULL);
+ pthread_create(&thread2, NULL, fun2, NULL);
+ pthread_join(thread1, NULL);
+ pthread_join(thread2,NULL);
+ printf("Final value of shared is %d\n",shared); //prints the last updated value of shared variable
+ return 0;
+ }
+void *fun1()
 {
-
-while(1){
-    if(counter==1){
-        sleep(2);
-    }else{
-        break;
-    }
+    int x;
+    sem_wait(&s); //executes wait operation on s
+    x=shared;//thread1 reads value of shared variable
+    printf("Thread1 reads the value as %d\n",x);
+    x++;  //thread1 increments its value
+    printf("Local updation by Thread1: %d\n",x);
+    sleep(1); //thread1 is preempted by thread 2
+    shared=x; //thread one updates the value of shared variable
+    printf("Value of shared variable updated by Thread1 is: %d\n",shared);    
+    sem_post(&s);
 }
-counter=1;
-x++;
-counter=0;
-
-	return NULL;
-}
-
-int main(void)
+void *fun2()
 {
-	int i = 0;
-	int error;
-
-	while (i < 2) {
-		error = pthread_create(&(tid[i]), NULL, &trythis, NULL);
-		if (error != 0)
-			printf("\nThread can't be created : [%s]", strerror(error));
-		i++;
-	}
-
-	pthread_join(tid[0], NULL);
-	pthread_join(tid[1], NULL);
-
-	return 0;
+    int y;
+    sem_wait(&s);
+    y=shared;//thread2 reads value of shared variable
+    printf("Thread2 reads the value as %d\n",y);
+    y--;  //thread2 increments its value
+    printf("Local updation by Thread2: %d\n",y);
+    sleep(1); //thread2 is preempted by thread 1
+    shared=y; //thread2 updates the value of shared variable
+    printf("Value of shared variable updated by Thread2 is: %d\n",shared);
+    sem_post(&s);
 }
